86c0ddc4bc6ea0b839deb65182c3d2d46510ffe8
Gernot Horak <gernot@horak.cc>
2022-05-11 19:14:14 +0200

    new file:   k8s/dns.tf 	new file:   k8s/variables.tf 	modified:   .gitignore 	modified:   k8s/.gitignore 	deleted:    k8s/db-secret.sh.old 	modified:   k8s/ingress-nginx-controller.yaml 	modified:   k8s/main.tf

---
+++ k8s/dns.tf
@@ -0,0 +1,40 @@
+
+provider "exoscale" {
+  alias  = "dns"
+  key    = "${var.exoscale_dns_api_key}"
+  secret = "${var.exoscale_dns_api_secret}"
+}
+
+resource "null_resource" "getsvcip" {
+  provisioner "local-exec" {
+    command      = "> ingress_ips.txt; until ! kubectl --kubeconfig kube_config get service --all-namespaces |grep LoadBalancer |grep ingress-nginx | grep pending; do echo still pending; sleep 5;done;  kubectl --kubeconfig kube_config get service --all-namespaces |grep LoadBalancer |grep ingress-nginx |awk '{print $5}' > ingress_ips.txt"
+  }
+  depends_on = [
+    resource.null_resource.k8s_app # optional if need to fit this in with other preceding resource
+  ]
+}
+
+data "local_file" "ingress-ip" {
+    filename = "ingress_ips.txt"
+  depends_on = [
+    resource.null_resource.getsvcip
+  ]
+}
+
+resource "exoscale_domain_record" "frontend-dns" {
+  provider    = exoscale.dns
+  domain      = "cldsvc.io"
+  name        = "order"
+  record_type = "A"
+  ttl         = "60"
+  content     = "${data.local_file.ingress-ip.content}"
+}
+
+resource "exoscale_domain_record" "backend-dns" {
+  provider    = exoscale.dns
+  domain      = "cldsvc.io"
+  name        = "exoscale-order-backend"
+  record_type = "A"
+  ttl         = "60"
+  content     = "${data.local_file.ingress-ip.content}"
+}
---
+++ k8s/variables.tf
@@ -0,0 +1,9 @@
+variable "project" { }
+variable "exoscale_api_key" { }
+variable "exoscale_api_secret" { }
+variable "exoscale_pgsql_user" { }
+variable "exoscale_pgsql_pwd" { }
+variable "zone" { }
+
+variable "exoscale_dns_api_key" { }
+variable "exoscale_dns_api_secret" { }
